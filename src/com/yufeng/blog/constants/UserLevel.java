51360f6450498140ffd422074df7dc1cf9d4f42b
YuFeng0023 <2028953318@qq.com>
2016-08-27 09:20:20 +0800

    Revert "Create"
    This reverts commit a7e2871d591186a3940648b5a92f9d0a94a61c61.

--- src/com/yufeng/blog/constants/UserLevel.java
+++
@@ -1,7 +0,0 @@
-package com.yufeng.blog.constants;
-
-public class UserLevel {
-	public static final int SUPER_MANAGER=-1;
-	public static final int VISITER=1;
-	public static final int MANAGER=2;
-}
--- src/com/yufeng/blog/model/PhotoClsf.java
+++
@@ -1,7 +0,0 @@
-package com.yufeng.blog.model;
-
-public class PhotoClsf {
-	protected String pcid;
-	protected String lable;
-	protected PhotoClsf fid; 
-}
